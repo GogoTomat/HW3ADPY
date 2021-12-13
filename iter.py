45c0c2f4047c72bc3027c51977ac3b4757348e83
GogoTomat <88964256+GogoTomat@users.noreply.github.com>
2021-12-13 23:19:03 +0500

    Update iter.py

--- iter.py
+++ iter.py
@@ -1,10 +1,24 @@
+class FlatIterator:
+
+    def __init__(self, nested_list):
+        self.nested_list = sum(nested_list,[])
+    
+    def __iter__(self):
+        self.i = -1
+        return self
+
+    def __next__(self):
+        self.i += 1
+        if self.i == len(self.nested_list,):
+            raise StopIteration
+        letter = self.nested_list[self.i]
+        return letter
+
 nested_list = [
-    ['a', 'b', 'c'],
-    ['d', 'e', 'f'],
-    [1, 2, None],
+	['a', 'b', 'c'],
+	['d', 'e', 'f'],
+	['g', 'h', 'i'],
 ]
 
-flatten = lambda x: [item for nested_list in x for item in nested_list]
-
-for i in flatten(nested_list):
-    print(i)
+for item in FlatIterator(nested_list):
+	print(item)
